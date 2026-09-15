0784c6e07ff8953c69c62e5653f2713a0adcd74d
sneh prajapti <sneh.propelius@gmail.com>
2026-09-15 12:47:37 +0530

    added event listener

--- counter.js
+++ counter.js
@@ -1,4 +1,9 @@
 let count = 0;
+document.getElementById("btnadd").addEventListener("click",increaseCount);
+document.getElementById("btnminus").addEventListener("click",decreaseCount);
+document.getElementById("btnreset").addEventListener("click",reset);
+document.getElementById("btnsave").addEventListener("click",savecount);
+document.getElementById("btnload").addEventListener("click",loadCount);
 
 function updateCounter(){
     document.getElementById('counter').innerHTML=count;
